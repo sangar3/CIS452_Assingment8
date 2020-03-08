721daae76c96a8790a4a91da78b4ffc179dadd54
sangar3 <55110746+sangar3@users.noreply.github.com>
2020-03-08 14:55:05 -0500

    first commit

---
+++ FPS/Assets/Pickup.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+    public GameObject pickedupgun;
+    public GameObject oldgun;
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Debug.Log("trigger");
+            pickedupgun.SetActive(true);
+            Debug.Log("picked up AK");
+            oldgun.SetActive(false);
+            Destroy(gameObject);
+        }
+
+    }
+}
---
+++ FPS/Assets/Scripts/Gun.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    [Header("Stats")]
+    public float damage = 10f;
+    public float range = 100f;
+    public float fireRate = 15f;
+    public float impactforce = 30f;
+    private float nextTimeToFire = 0f;
+
+    
+    [Header("Fields")]
+    public ParticleSystem muzzleflash;
+    public GameObject impacteffect;
+    public Camera fpsCam;
+
+   
+    void Update()
+    {
+        if(Input.GetButton("Fire1") && Time.time >= nextTimeToFire)
+        {
+            
+            nextTimeToFire = Time.time + 1f / fireRate;  //setting firerate
+            Shoot();
+
+        }
+    }
+
+
+    void Shoot()
+    {
+        muzzleflash.Play();
+        RaycastHit hit;
+        if(Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+
+            Target target = hit.transform.GetComponent<Target>();
+            if(target != null)
+            {
+                target.TakeDamage(damage);
+            }
+
+            if(hit.rigidbody != null)
+            {
+                hit.rigidbody.AddForce(-hit.normal * impactforce);
+            }
+           GameObject impactGO =  Instantiate(impacteffect, hit.point, Quaternion.LookRotation(hit.normal));
+            Destroy(impactGO, 2f);
+        }
+    }
+}
